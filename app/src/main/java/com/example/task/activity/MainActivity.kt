2c50b2e7a75d5432f8c8f26632ef287240822999
souravnoobcoder <souravrawat77777777@gmail.com>
2021-11-09 17:52:41 +0530

    initial commit

---
+++ app/src/main/java/com/example/task/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.task.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.task.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
